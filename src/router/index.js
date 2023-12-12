dbef02a2b276ba47bc38fc6606fd671379f39a36
moapochta06 <90915948+moapochta06@users.noreply.github.com>
2023-12-13 01:13:57 +0700

    add changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'; 
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+export default createRouter({
+    history: createWebHistory(),
+    routes:[
+        {
+            path: '/', 
+            name: 'home',
+            component: ()=> import('@/pages/news-list/news-list-page.vue')
+        },
+        {
+            path: '/news/:id',
+            name: 'news',
+            component: ()=> import('@/pages/news-pages/news-page.vue'),
+            params: true
+        }
+    ]
 })
-
-export default router
